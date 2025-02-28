a1caa5bc0e40d1cc9931e70bdafe0c88c7e7885e
TariqBazadough <tariq.bazadough@gmail.com>
2025-02-28 19:55:55 +0300

    I have changed the following: 1- function description 2- function declartion type 3- added example

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,14 +4,26 @@
  */
 const numbers = [10, 13, 20, 25, 38, 35, 40];
 
-// Using `squaredNumbers` function return a new array that contains each number squared.
-function squaredNumbers(numbers) {
+// Using `squaredNumbers` function that:
+// - Accepts "numbers" parameter of type "number[]".
+// - Return a new array that contains each number squared.
+// example:
+// squaredNumbers(numbers); // => [100, 169, 400, 625, 1444, 1225, 1600]
+function squaredNumbers(numbers: number[]): number[] {
   // write your code here...
+
+  return []; // replace empty array with what you see is fit
 }
 
-// Using `doubledNumbers` function return a new array that contains each number multiplied by 2.
-function doubledNumbers(numbers) {
+// Using `doubledNumbers` function that:
+// - Accepts "numbers" parameter of type "number[]".
+// - Return a new array that contains each number multiplied by 2.
+// example:
+// doubledNumbers(numbers); // => [20, 26, 40, 50, 76, 70, 80]
+function doubledNumbers(numbers: number[]): number[] {
   // write your code here...
+
+  return []; // replace empty array with what you see is fit
 }
 
 export { squaredNumbers, doubledNumbers };
